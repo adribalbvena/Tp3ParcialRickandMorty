aad7f246f21f270dcd32e5981aa213c2d7ba1f1e
Adriana Balbuena <adribalbuena@hotmail.com>
2022-11-04 02:57:45 +0100

    images and glide added

--- app/src/main/java/ar/edu/ort/tp3parcialrickandmorty/holders/HomeViewHolder.kt
+++ app/src/main/java/ar/edu/ort/tp3parcialrickandmorty/holders/HomeViewHolder.kt
@@ -1,23 +1,31 @@
 package ar.edu.ort.tp3parcialrickandmorty.holders
 
 import android.view.View
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import ar.edu.ort.tp3parcialrickandmorty.R
 import ar.edu.ort.tp3parcialrickandmorty.data.Character
+import com.bumptech.glide.Glide
 
 class HomeViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
     private val name: TextView
     private val status: TextView
+    private val image: ImageView
 
     init {
         name = itemView.findViewById(R.id.tvNameCharacter)
         status = itemView.findViewById(R.id.tvStatusCharacter)
+        image = itemView.findViewById(R.id.ivCharacter)
     }
 
     fun bind(character: Character) {
         name.text = character.name
         status.text = character.status
+
+        Glide.with(itemView)
+            .load(character.image)
+            .into(image)
     }
 
 }
