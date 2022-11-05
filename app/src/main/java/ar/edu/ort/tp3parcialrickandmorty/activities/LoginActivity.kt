8532640ecf43034d0092d0ea1376b8d83af8ab2c
Nicolás Javier Billán Danylyszyn <billan_njd@hotmail.com>
2022-11-05 15:40:45 -0300

    Login Activity

---
+++ app/src/main/java/ar/edu/ort/tp3parcialrickandmorty/activities/LoginActivity.kt
@@ -0,0 +1,44 @@
+package ar.edu.ort.tp3parcialrickandmorty.activities
+
+import android.content.Intent
+import android.os.Bundle
+import android.text.TextUtils
+import androidx.appcompat.app.AppCompatActivity
+import ar.edu.ort.tp3parcialrickandmorty.databinding.ActivityLoginBinding
+import ar.edu.ort.tp3parcialrickandmorty.sessionmanager.SessionManager
+
+class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        var username = SessionManager(this).fetchUserName()
+
+        if (username != null && !TextUtils.isEmpty(username)) {
+            startMainActivity()
+        }
+
+        binding.buttonLoginSignin.setOnClickListener { onLoginButtonClick() }
+    }
+
+    private fun onLoginButtonClick() {
+        val username = binding.editTextLoginUsername.text.toString()
+        val password = binding.editTextLoginPassword.text.toString()
+
+        if (TextUtils.isEmpty(username) || TextUtils.isEmpty((password))) {
+            return
+        }
+
+        SessionManager(this).saveUserName(username)
+        startMainActivity()
+    }
+
+    private fun startMainActivity() {
+        this.startActivity(Intent(this, MainActivity::class.java))
+        this.finish()
+    }
+}
